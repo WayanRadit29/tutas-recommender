7afdb1f57eee2e0c64db094a611098e7563245e3
WayanRadit29 <radityaputraa1122@gmail.com>
2025-09-03 14:01:48 +0700

    Delete submit.py file

--- training/submit.py
+++
@@ -1,9 +0,0 @@
-import tensorflow_cloud as tfc
-
-tfc.run(
-    entry_point="train.py",
-    requirements_txt="requirements.txt",
-    docker_config=tfc.DockerConfig(parent_image="tensorflow/tensorflow:2.15.0"),
-    chief_config=tfc.COMMON_MACHINE_CONFIGS["CPU"], 
-    stream_logs= True
-)
